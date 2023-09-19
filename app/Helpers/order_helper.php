54df4f35851e120704125cb15194fa373b388f7f
ruligandari <ruligandari@gmail.com>
2023-09-19 21:34:17 +0700

    fix generateNoOrder

--- app/Helpers/order_helper.php
+++ app/Helpers/order_helper.php
@@ -4,25 +4,25 @@ namespace App\Helpers;
 
 use App\Models\OrderModel;
 
-function generateNoOrder ($tahun){
+function generateNoOrder($tahun)
+{
     // mengambil data dari Model Order kemudian mengambil data terakhir dari no_order
     $order = new OrderModel();
     $dataOrder = $order->orderBy('no_order', 'DESC')->first();
-    if(empty($dataOrder)){
+    if (empty($dataOrder)) {
         $nomorTerakhir = "1";
 
-        $noorder = sprintf("%03d",$nomorTerakhir);
-        $order = "ORD/".$noorder."/".$tahun;
+        $noorder = sprintf("%03d", $nomorTerakhir);
+        $order = "ORD/" . $noorder . "/" . $tahun;
         return $order;
     } else {
         // ambil nomor transaksi dari 001/KRAB18/2023
         $nomorTerakhir = explode('/', $dataOrder['no_order']);
         $nomor = $nomorTerakhir[1];
-        echo $nomor;
         $newNumber = $nomor + 1;
 
-        $noorder = sprintf("%03d",$newNumber);
-        $order = "ORD/".$noorder."/".$tahun;
+        $noorder = sprintf("%03d", $newNumber);
+        $order = "ORD/" . $noorder . "/" . $tahun;
         return $order;
     }
 }
